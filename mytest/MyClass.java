1eac4e4bfe5cf4803d201a94f651470be4efb811
Yueur Lami <yueur.lami@gmail.com>
2018-05-01 12:17:47 +0200

    Added Java code

---
+++ mytest/MyClass.java
@@ -0,0 +1,16 @@
+package mytest;
+
+public class MyClass{
+	public static void main(String[] argv){
+		
+	}
+	
+	public int add(int _a, int _b){
+		return _a+_b;
+	}
+	
+	public double add(double _a, double _b){
+		return _a+_b;
+	}
+}
+
---
+++ mytest/tests/TestMyClass.java
@@ -0,0 +1,12 @@
+package mytest.tests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestMyClass extends TestCase{
+	@Test
+	public void test() {
+		assertTrue(true);
+		assertFalse(false);
+	}
+}
